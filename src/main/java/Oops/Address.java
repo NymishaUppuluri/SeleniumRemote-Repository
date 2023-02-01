938bb4cf0e4b9272d6d82e1cf27d2db9e4bf2cd5
nyuppuluri <nymisha480@gmail.com>
2023-02-01 12:51:36 +0530

    new commit

---
+++ src/main/java/Oops/Address.java
@@ -0,0 +1,41 @@
+package Oops;
+
+public class Address {
+
+
+
+    String DoorNumber;
+ String StreetNumber;
+ String City;
+ int PinCode;
+
+ Address() {
+     String DoorNumber = "1-103";
+     String StreetNumber = "1";
+     String City = "Visakhapatnam";
+     int PinCode = 531173;
+
+
+     System.out.println(DoorNumber);
+     System.out.println(StreetNumber);
+     System.out.println(City);
+     System.out.println(PinCode);
+ }
+ public static void main(String[]args) {
+     Address address = new Address();
+
+
+
+
+ }
+
+
+
+
+
+}
+
+
+
+
+
