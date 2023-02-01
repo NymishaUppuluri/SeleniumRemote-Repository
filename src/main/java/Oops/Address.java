44b3d3bb2fdb226c7dc9eba28c860ab545c92288
nyuppuluri <nymisha480@gmail.com>
2023-02-01 15:21:17 +0530

    new commit

--- src/main/java/Oops/Address.java
+++ src/main/java/Oops/Address.java
@@ -3,17 +3,18 @@ package Oops;
 public class Address {
 
 
-
     String DoorNumber;
  String StreetNumber;
  String City;
  int PinCode;
 
  Address() {
-     String DoorNumber = "1-103";
-     String StreetNumber = "1";
-     String City = "Visakhapatnam";
-     int PinCode = 531173;
+     DoorNumber = "1-103";
+     StreetNumber = "1";
+     City = "Visakhapatnam";
+     PinCode = 531173;
+ }
+ void  printAddress(){
 
 
      System.out.println(DoorNumber);
@@ -23,6 +24,18 @@ public class Address {
  }
  public static void main(String[]args) {
      Address address = new Address();
+Address address1 = new Address();
+System.out.println("Initial values of Address in address");
+address.printAddress();
+System.out.println("Initial Address values in address1");
+address1.printAddress();
+System.out.println("Updated Address values in address1");
+address1.DoorNumber = "1-203";
+address1.StreetNumber = "23";
+address1.City = "Hyd";
+address1.PinCode = 5300089;
+     address1.printAddress();
+
 
 
 
